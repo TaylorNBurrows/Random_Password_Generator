e78e95ce787bb97364dc331f992def4933058149
TMonies <15614796+TaylorNBurrows@users.noreply.github.com>
2019-10-08 22:48:20 -0700

    made changes to global variables to remove them from the function

--- script2.js
+++ script2.js
@@ -1,20 +1,33 @@
-document.querySelector("#generate").addEventListener("click", function() {
-    var numberOfChar = prompt("How many characters do you want the password to be?")
-    var hasSpecialChar = confirm("Do you want special characters in the password?")
-    var hasNumericChar = confirm("Do you want numbers in the password?")
-    var hasUpperChar = confirm("Do you want upper case letters in the password?")
-    var hasLowerChar = confirm("Do you want lower case letters in the password?")
-    genPassword(numberOfChar, hasSpecialChar, hasNumericChar, hasUpperChar, hasLowerChar)
+var numberOfChar;
+var hasSpecialChar;
+var hasNumericChar;
+var hasUpperChar;
+var hasLowerChar;
+
+document.querySelector("#generateBtn").addEventListener("click", function() {
+    numberOfChar = prompt("How many characters do you want the password to be?")
+    hasSpecialChar = confirm("Do you want special characters in the password?")
+    hasNumericChar = confirm("Do you want numbers in the password?")
+    hasUpperChar = confirm("Do you want upper case letters in the password?")
+    hasLowerChar = confirm("Do you want lower case letters in the password?")
+    generate(numberOfChar, hasSpecialChar, hasNumericChar, hasUpperChar, hasLowerChar)
 })
+
 var special = "!@#$%^&*()+=.~".split("");
 var numbers = "1234567890".split("");
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var lower = "abcdefghijklmnopqrstuvwxyz".split("");
+
+
 function getRandomValue(arr) {
     var randomIndex = Math.floor((Math.random) * arr.length)
     return arr[randomIndex]
 }
-function genPassword(numberOfChar, hasSpecialChar, hasNumericChar, hasUpperChar, hasLowerChar) {
+
+
+var passwordResult = ""
+
+function generate(numberOfChar, hasSpecialChar, hasNumericChar, hasUpperChar, hasLowerChar) {
     var randomSpecialChar;
     var randomNumericChar;
     var randomUpperChar;
@@ -26,10 +39,24 @@ function genPassword(numberOfChar, hasSpecialChar, hasNumericChar, hasUpperChar,
         randomNumericChar = getRandomValue(numbers)
     }
     if (hasUpperChar) {
-        randomUpperChar = getRandomValue(upper)
+        randomUpperChar= getRandomValue(upper)
     }
     if (hasLowerChar) {
         randomLowerChar = getRandomValue(lower)
     }
+    
+    passwordResult = randomSpecialChar + randomNumericChar + randomUpperChar + randomLowerChar
+
+    document.querySelector("#password").textContent = passwordResult;
+    
+
     }
+    
+//copy to clipboard
+function copyBtn() {
+    document.getElementById("password").select();
+
+    document.execCommand("Copy");
+
+    alert("Your password " + generatedPassword + "has been successfully copied to the clipboard")
 }
